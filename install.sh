018b30f41abb2d15e58bd520ec9a9e59b331190a
joaberg <58996735+joaberg@users.noreply.github.com>
2023-08-21 11:59:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ echo "test 2"
 
 if [ $? -eq 0 ]; then # only run if previous command is a succuess.
     # Add import of the zsh.nix
-    #config_file=".config/home-manager/home.nix"; line_number=$(grep -n "home.stateVersion" "$config_file" | cut -d: -f1); text_to_add="imports = [./zsh.nix];"; if [ -n "$line_number" ]; then sed -i "$((line_number + 1)),$((line_number + 1))i\\$text_to_add\\" "$config_file"; else echo "Line not found in the file."; fi 
+    config_file=".config/home-manager/home.nix"; line_number=$(grep -n "home.stateVersion" "$config_file" | cut -d: -f1); text_to_add="imports = [./zsh.nix];"; if [ -n "$line_number" ]; then sed -i "$((line_number + 1)),$((line_number + 1))i\\$text_to_add\\" "$config_file"; else echo "Line not found in the file."; fi 
     
     #Download the zsh.nix
     curl -o ~/.config/home-manager/zsh.nix https://raw.githubusercontent.com/joaberg/server-zsh-nix/main/zsh.nix
