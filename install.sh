d3f3579f2c0f9297e1f2a0a827e8ce61b4f1e083
joaberg <58996735+joaberg@users.noreply.github.com>
2023-08-21 11:57:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,22 +3,26 @@
 # Install nix and home-manager
 #curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
+echo "test 1"
+
 if [ $? -eq 0 ]; then # only run if previous command is a succuess.
     source .bashrc 
     #nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager 
     #nix-channel --update 
     #nix-shell '<home-manager>' -A install
 fi
-echo "test"
+
+echo "test 2"
 
 if [ $? -eq 0 ]; then # only run if previous command is a succuess.
     # Add import of the zsh.nix
     #config_file=".config/home-manager/home.nix"; line_number=$(grep -n "home.stateVersion" "$config_file" | cut -d: -f1); text_to_add="imports = [./zsh.nix];"; if [ -n "$line_number" ]; then sed -i "$((line_number + 1)),$((line_number + 1))i\\$text_to_add\\" "$config_file"; else echo "Line not found in the file."; fi 
     
     #Download the zsh.nix
-    curl -o ~/.config/home-manager/zsh.nix https://github.com/joaberg/server-zsh-nix/raw/main/zsh.nix
+    curl -o ~/.config/home-manager/zsh.nix https://raw.githubusercontent.com/joaberg/server-zsh-nix/main/zsh.nix
+
 fi
 
 
-home-manager switch
+#home-manager switch
 
