4d9956a6794712639396f45eda6f2aeb092d9204
joaberg <58996735+joaberg@users.noreply.github.com>
2023-08-21 12:41:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Install nix and home-manager
-#curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
-echo "test 1"
 
 if [ $? -eq 0 ]; then # only run if previous command is a succuess.
     source .bashrc 
@@ -12,12 +11,21 @@ if [ $? -eq 0 ]; then # only run if previous command is a succuess.
     #nix-shell '<home-manager>' -A install
 fi
 
-echo "test 2"
 
 if [ $? -eq 0 ]; then # only run if previous command is a succuess.
     # Add import of the zsh.nix
-    config_file=".config/home-manager/home.nix"; line_number=$(grep -n "home.stateVersion" "$config_file" | cut -d: -f1); text_to_add="imports = [./zsh.nix];"; if [ -n "$line_number" ]; then sed -i "$((line_number + 1)),$((line_number + 1))i\\$text_to_add\\" "$config_file"; else echo "Line not found in the file."; fi 
+    #config_file=".config/home-manager/home.nix"; line_number=$(grep -n "home.stateVersion" "$config_file" | cut -d: -f1); text_to_add="imports = [./zsh.nix];"; if [ -n "$line_number" ]; then sed -i "$((line_number + 1)),$((line_number + 1))i\\$text_to_add\\" "$config_file"; else echo "Line not found in the file."; fi 
+    config_file=".config/home-manager/home.nix"
+    line_number=$(grep -n "home.stateVersion" "$config_file" | cut -d: -f1)
+    text_to_add="imports = [./zsh.nix];"
     
+    if [ -n "$line_number" ]; then
+        # Check if the text already exists in the file
+        if ! grep -qF "$text_to_add" "$config_file"; then
+            # Add the text after the specified line number
+            sed -i "$((line_number + 1))i\\$text_to_add" "$config_file"
+        fi
+    fi
     #Download the zsh.nix
     curl -o ~/.config/home-manager/zsh.nix https://raw.githubusercontent.com/joaberg/server-zsh-nix/main/zsh.nix
 
