78e12238d2b604591f021941e1b04905b6649129
joaberg <58996735+joaberg@users.noreply.github.com>
2023-08-21 13:25:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Install nix and home-manager
-#curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
-echo 1
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
 if [ $? -eq 0 ]; then # only run if previous command is a succuess.
     source .bashrc 
@@ -12,7 +11,6 @@ fi
 
 if [ $? -eq 0 ]; then # only run if previous command is a succuess.
     # Add import of the zsh.nix
-    #config_file=".config/home-manager/home.nix"; line_number=$(grep -n "home.stateVersion" "$config_file" | cut -d: -f1); text_to_add="imports = [./zsh.nix];"; if [ -n "$line_number" ]; then sed -i "$((line_number + 1)),$((line_number + 1))i\\$text_to_add\\" "$config_file"; else echo "Line not found in the file."; fi 
     config_file=".config/home-manager/home.nix"
     line_number=$(grep -n "home.stateVersion" "$config_file" | cut -d: -f1)
     text_to_add="imports = [./zsh.nix];"
