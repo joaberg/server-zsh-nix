c439c5b1d46b5aea2a51b4c13e06a06168d6b9ec
joaberg <58996735+joaberg@users.noreply.github.com>
2023-08-23 10:17:22 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+#Download the latest zsh.nix
+curl -o ~/.config/home-manager/zsh.nix https://raw.githubusercontent.com/joaberg/server-zsh-nix/main/zsh.nix
+
+# Rebuild
+home-manager switch
