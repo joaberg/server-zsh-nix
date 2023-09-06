b72ce1dc64b92e4ea10e9402bb758fd022d90427
joaberg <58996735+joaberg@users.noreply.github.com>
2023-09-06 11:52:16 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@
 curl -o ~/.config/home-manager/zsh.nix https://raw.githubusercontent.com/joaberg/server-zsh-nix/main/zsh.nix
 
 # Rebuild
-home-manager switch -b backup
+nix-channel --update && home-manager switch -b backup
